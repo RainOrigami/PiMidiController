2b22140daab363f3610005e9c315c15504c626fd
Longoon12000 <rain+github@codefreak.net>
2023-04-27 11:12:25 +0200

    Adds parsing of sysex color midi messages

--- PiControllerServer/Midi.cs
+++ PiControllerServer/Midi.cs
@@ -1,6 +1,7 @@
 ﻿using NAudio.Midi;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +11,20 @@ namespace PiControllerServer
 {
     internal class Midi : IDisposable
     {
+        private static Guid manufacturer = new Guid("e11d4d2a-9bdc-4ac4-afea-ee5907a8dbfc");
+        private static Guid product = new Guid("66a94d67-e211-49fb-b410-c6a011ddddc5");
+
         private TeVirtualMIDI device;
 
         public event EventHandler<MidiEvent>? MidiEventReceived;
+        public event EventHandler<(int note, int red, int green, int blue)>? ColorEventReceived;
 
         public Midi(string deviceName)
         {
-            this.device = new TeVirtualMIDI(deviceName, flags: 0);
+#if DEBUG
+            TeVirtualMIDI.logging(TeVirtualMIDI.TE_VM_LOGGING_RX | TeVirtualMIDI.TE_VM_LOGGING_TX | TeVirtualMIDI.TE_VM_LOGGING_MISC);
+#endif
+            this.device = new TeVirtualMIDI(deviceName, ushort.MaxValue, TeVirtualMIDI.TE_VM_FLAGS_PARSE_RX | TeVirtualMIDI.TE_VM_FLAGS_INSTANTIATE_BOTH, ref manufacturer, ref product);
 
             Task.Run(handle);
         }
@@ -26,12 +34,25 @@ namespace PiControllerServer
             while (true)
             {
                 byte[] messageBytes = this.device.getCommand();
-                byte[] paddedMessageBytes = new byte[Math.Max(messageBytes.Length, 4)];
-                messageBytes.CopyTo(paddedMessageBytes, 0);
+                if (messageBytes.Length == 3)
+                {
+                    try
+                    {
+                        byte[] paddedMessageBytes = new byte[4];
+                        messageBytes.CopyTo(paddedMessageBytes, 0);
 
-                MidiEvent midiEvent = MidiEvent.FromRawMessage(BitConverter.ToInt32(paddedMessageBytes, 0));
-
-                this.MidiEventReceived?.Invoke(this, midiEvent);
+                        MidiEvent midiEvent = MidiEvent.FromRawMessage(BitConverter.ToInt32(paddedMessageBytes, 0));
+                        this.MidiEventReceived?.Invoke(this, midiEvent);
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex);
+                    }
+                }
+                else if (messageBytes.Length == 6)
+                {
+                    this.ColorEventReceived?.Invoke(this, ((int)messageBytes[1], (int)messageBytes[2], (int)messageBytes[3], (int)messageBytes[4]));
+                }
             }
         }
 
